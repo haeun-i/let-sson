453e3e94d93383d0d508afa1e2e747255823fac1
seokchae05 <72611748+seokchae05@users.noreply.github.com>
2021-05-16 16:05:20 +0900

    헤더 및 아이디 비번찾기만 css

--- front/src/page/loginsuccess.js
+++ front/src/page/loginsuccess.js
@@ -1,74 +1,81 @@
 import React from "react";
-import HeadButton from "../component/layout/header/header"
+import HeadButton from "../component/layout/header/header";
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
-    background-color : #F6F4F3;
-    position: absolute;
-    width: 100%; 
-    height: 90%; 
-    display : flex;
-    flex-direction: column;
-    align-items : center;
-    justify-content: center;
+  background-color: #f6f4f3;
+  position: absolute;
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 const SuccessText = styled.div`
-    font-size : 25px;
-    padding-right : 30px;
-    margin-bottom : 5%;
+  position: absolute;
+  width: 231px;
+  height: 35px;
+
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 29px;
+
+  color: #02001e;
+  padding-right: 30px;
+  margin-bottom: 5%;
 `;
 
 const SuccessBtn1 = styled.button`
-    border-top : solid 3px #010440;
-    border-left : none;
-    border-right : none;
-    border-bottom : none;
-    background-color: white;
-    font-size: 16px;
-    margin-top : 30px;
-    margin-left : 30px;
-    margin-right: 150px;
-    margin-bottom : 30px;
-    padding : 0 15px 0 15px;
+  border-top: solid 3px #010440;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+  background-color: white;
+  font-size: 16px;
+  margin-top: 30px;
+  margin-left: 30px;
+  margin-right: 150px;
+  margin-bottom: 30px;
+  padding: 0 15px 0 15px;
 `;
 
 const SuccessBtn2 = styled.button`
-    border-top : solid 3px #010440;
-    border-left : none;
-    border-right : none;
-    border-bottom : none;
-    background-color: white;
-    font-size: 16px;
-    margin-top : 30px;
-    margin-left : 30px;
-    margin-right: 30px;
-    margin-bottom : 30px;
-    padding : 0 15px 0 15px;
+  border-top: solid 3px #010440;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+  background-color: white;
+  font-size: 16px;
+  margin-top: 30px;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-bottom: 30px;
+  padding: 0 15px 0 15px;
 `;
 
-class Loginsuccess extends React.Component{
-    render(){
-        return(
-            <div>
-                <HeadButton />
-                <Wrapper>
-                    <SuccessText>
-                        로그인이 완료 되었습니다.
-                    </SuccessText>
-                    <div>
-                        <Link to="/findteacher">
-                            <SuccessBtn1>선생님 찾으러가기</SuccessBtn1>
-                        </Link>
-                        <Link to="/findstudent">
-                            <SuccessBtn2>학생 찾으러가기</SuccessBtn2>
-                        </Link>
-                        
-                    </div>
-                </Wrapper>
-            </div>
-        );
-    }
+class Loginsuccess extends React.Component {
+  render() {
+    return (
+      <div>
+        <HeadButton />
+        <Wrapper>
+          <SuccessText>로그인이 완료 되었습니다.</SuccessText>
+          <div>
+            <Link to="/findteacher">
+              <SuccessBtn1>선생님 찾으러가기</SuccessBtn1>
+            </Link>
+            <Link to="/findstudent">
+              <SuccessBtn2>학생 찾으러가기</SuccessBtn2>
+            </Link>
+          </div>
+        </Wrapper>
+      </div>
+    );
+  }
 }
 
 export default Loginsuccess;
