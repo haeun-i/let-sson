0c05f6d2987e915246cb5286394b208e4a5d24ca
seokchae05 <72611748+seokchae05@users.noreply.github.com>
2021-07-19 00:28:13 +0900

    모바일 학생m페이지 사이드바 수정

--- front/src/component/shared/myPageS/sidebarMyPs.js
+++ front/src/component/shared/myPageS/sidebarMyPs.js
@@ -8,7 +8,8 @@ const Wrapper = styled.div`
   border: 1px solid white;
   display: flex;
   position: absolute;
-  width: 49%;
+  width: 40%;
+  margin-left:10px;
   background-color: white;
   height: 36px;
   background: none;
@@ -21,7 +22,7 @@ const SideB = styled.button`
   font-size: 15px;
   border: solid 1px #e8e8e8;
   padding: 10px;
-  width: 120px;
+  width: 110px;
   height: 36px;
   margin: 0;
 `;
@@ -30,7 +31,7 @@ const Btn = styled.button`
   font-size: 15px;
   border: solid 1px #e8e8e8;
   padding: 10px;
-  width: 120px;
+  width: 110px;
   height: 36px;
   margin: 0;
 `;
